a309c3b981ba5756b349d6f4ddf8931446c14018
UIT-21522420 <dipper21333@gmail.com>
2023-12-11 17:32:59 +0700

    v1.0

---
+++ OnlineEDUschoolManaementSystemMVC/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using OnlineEDUschoolManaementSystemMVC.Models;
+
+namespace OnlineEDUschoolManaementSystemMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Courses()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(TblUser r)
+        {
+            using (EDUDataEntities db = new EDUDataEntities())
+            {
+                if (ModelState.IsValid)
+                {
+                    db.TblUsers.Add(r);
+                    db.SaveChanges();
+                    //  ViewBag.message("Register Sucess");
+                    ModelState.Clear();
+                }
+            }
+            return View(r);
+        }
+        [HttpPost]
+        public ActionResult Login(TblUser r1)
+        {
+            using (EDUDataEntities db = new EDUDataEntities())
+            {
+                if (ModelState.IsValid)
+                {
+                    var log = db.TblUsers.Where(a => a.userName.Equals(r1.userName) && a.password.Equals(r1.password)).FirstOrDefault();
+                    if (log != null)
+                    {
+                        return RedirectToAction("Contact");
+                    }
+                    db.SaveChanges();
+                    // ViewBag.message("Login Sucess");
+                    ModelState.Clear();
+                }
+            }
+            return View(r1);
+        }
+
+    }
+}
---
+++ OnlineEDUschoolManaementSystemMVC/Models/HeThong/Common.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+
+namespace OnlineEDUschoolManaementSystemMVC.Models
+{
+    public class MapCourses
+    {
+        DBEntities db = new DBEntities();
+        //1. Danh sach
+        public List<CourseTbl> CourseTbls()
+        {
+            try 
+            {
+                var listCourse = db.CourseTbls.ToList();
+                return listCourse;
+            }
+            catch 
+            {
+                return new List<CourseTbl>();
+            }
+        }   
+        //2. Chi tiet
+        public CourseTbl ChiTiet(int courseid)
+        {
+            try
+            {
+                return db.CourseTbls.Find(courseid);
+            }
+            catch
+            {
+                return new CourseTbl();
+            }
+        }
+        //3. Them moi
+        public int ThemMoi(CourseTbl newModel)
+        {
+            try
+            {
+                db.CourseTbls.Add(newModel);
+                db.SaveChanges();
+                return newModel.courseId;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+        //4. Cap nhat
+        public bool CapNhat(CourseTbl updateModel) 
+        {
+            try
+            {
+                // 1. Tim doi tuong can cap nhat
+                var khoaHoc = db.CourseTbls.Find(updateModel.courseId);
+                khoaHoc.courseId = updateModel.courseId;
+                khoaHoc.courseName = updateModel.courseName;
+                khoaHoc.coursePrice = updateModel.coursePrice;
+                khoaHoc.description = updateModel.description;
+                khoaHoc.discount = updateModel.discount;
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        //5. Xoa
+        public bool Xoa(int maKhoaHoc)
+        {
+            try
+            {
+                var khoaHoc = db.CourseTbls.Find(maKhoaHoc);
+                db.CourseTbls.Remove(khoaHoc);
+                db.SaveChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+    }
+}
